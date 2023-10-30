e73034a6fc901e8953a321eec64ba3c6ccfe392e
Chia1104 <yuyuchia7423@gmail.com>
2023-10-30 22:29:56 +0800

    test: test 0.0

--- infra/zeabur/build.sh
+++ infra/zeabur/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p .zeabur/output/index.func && \
-cp -r dist/ .zeabur/output/index.func/ && \
-cp -r package.json .zeabur/output/index.func/ && \
-mv .zeabur/output/index.func/lambda.js .zeabur/output/index.func/index.js
+mkdir -p .zeabur/output/functions/index.func && \
+cp -r dist/ .zeabur/output/functions/index.func/ && \
+cp -r package.json .zeabur/output/functions/index.func/ && \
+mv .zeabur/output/functions/index.func/lambda.js .zeabur/output/functions/index.func/index.js
