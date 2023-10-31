2a0aa6d791591d76ee2dae980c0de09214f78367
Chia1104 <yuyuchia7423@gmail.com>
2023-10-31 21:26:43 +0800

    test: test zeabur.sls

---
+++ src/zeabur.sls.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+let cachedServer: any;
+
+async function bootstrap() {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+
+    nestApp.enableCors();
+
+    cachedServer = await nestApp.init();
+  }
+
+  return cachedServer;
+}
+
+export const handler = async (ctx: any) => {
+  return await bootstrap();
+};
