08d8707f64bef8726478b2056081b8198e2a350e
Chia1104 <yuyuchia7423@gmail.com>
2023-11-01 14:38:36 +0800

    test: remove node_modules

--- infra/vercel/post-build.js
+++ infra/vercel/post-build.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
 // copy node_modules into function output directory
-console.info('Copying node_modules into .vercel/output/functions/index.func/node_modules')
-fs.cpSync('node_modules', '.vercel/output/functions/index.func/node_modules', {recursive: true, verbatimSymlinks: true})
+// console.info('Copying node_modules into .vercel/output/functions/index.func/node_modules')
+// fs.cpSync('node_modules', '.vercel/output/functions/index.func/node_modules', {recursive: true, verbatimSymlinks: true})
 
 // copy package.json into function output directory
-console.info('Copying package.json into .vercel/output/functions/index.func')
-fs.cpSync('package.json', '.vercel/output/functions/index.func/package.json')
+// console.info('Copying package.json into .vercel/output/functions/index.func')
+// fs.cpSync('package.json', '.vercel/output/functions/index.func/package.json')
 
 // copy .vc-config.json into function output directory
 console.info('Copying .vc-config.json into .vercel/output/functions/index.func')
